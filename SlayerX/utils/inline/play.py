a513695a259ab042bdb32335ce12cfbb7d74ca66
UnknownDz <111676071+manish-k0@users.noreply.github.com>
2024-05-10 13:16:37 +0530

    Update play.py

--- SlayerX/utils/inline/play.py
+++ SlayerX/utils/inline/play.py
@@ -68,14 +68,6 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
-        [
-            InlineKeyboardButton(
-                text="✭ ᴜᴘᴅᴀᴛᴇs ✭", url=config.SUPPORT_CHANNEL
-            ),
-            InlineKeyboardButton(
-                text="✭ sᴜᴘᴘᴏʀᴛ ✭", url=config.SUPPORT_CHAT
-            )
-        ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
